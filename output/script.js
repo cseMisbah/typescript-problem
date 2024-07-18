152ee4ad9b48e848374c5fa22745d000d312350e
Md Mijbah <mijbahsakib@gmail.com>
2024-07-18 12:07:14 +0600

    first - project

---
+++ output/script.js
@@ -0,0 +1,4 @@
+"use strict";
+//console.log("Hello");
+const country = "I love my country Bangladesh a lot";
+console.log(country);
---
+++ src/script.ts
@@ -0,0 +1,4 @@
+//console.log("Hello");
+
+const country = "I love my country Bangladesh a lot";
+console.log(country);
