671c8178da49190525649967b4754f366d21ccbb
Md Mijbah <mijbahsakib@gmail.com>
2024-07-18 12:37:40 +0600

    challenge-2

---
+++ output/script2.js
@@ -0,0 +1,12 @@
+"use strict";
+//let fruits = ["apple", "banana", "orange"];
+//fruits.push(30);
+//fruits.push("jackfruit");
+let person = {
+    name: "Mashrafi",
+    age: 35,
+    isCaptain: true,
+};
+person.country = "India";
+person.name = "Rifat";
+console.log(person);
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,28 @@
 //console.log("Hello");
 
-const country = "I love my country Bangladesh a lot";
-console.log(country);
+//const country = "I love my country Bangladesh a lot";
+//console.log(country);
+
+//let playerName = "Mashrafi";
+//console.log(playerName);
+
+//let age = 28;
+
+//age = "sakib";
+//playerName = 30;
+//console.log(playerName);
+
+/*
+let playerName;
+console.log(playerName);
+playerName = 34;
+console.log(playerName);
+playerName = "Ajij";
+console.log(playerName);
+*/
+
+function multiply(a: number, b: number) {
+  return a * b;
+}
+
+console.log(multiply(3, 4));
